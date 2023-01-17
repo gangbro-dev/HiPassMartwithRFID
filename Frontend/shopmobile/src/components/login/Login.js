49588044c8759bf87c52f5259e3990bbb48d2248
정시언 <rudtn0820@naver.com>
2023-01-17 17:58:29 +0900

    로그인회원가입진행중

---
+++ Frontend/shopmobile/src/components/login/Login.js
@@ -0,0 +1,125 @@
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#0971f1',
+      darker: '#053e85',
+    },
+    kakao: {
+      main: '#FEE500',
+      darker: '#053e85',
+      
+    },
+    naver: {
+      main: '#2DB400',
+      darker: '#053e85',
+      contrastText: '#fff',
+    },
+  },
+});
+
+export default function SignIn() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("userid"),
+      password: data.get("password"),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar src="./images/logo.png" sx={{ mb: 2 }} />
+          <Typography component="h1" variant="h5">
+            로그인
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="userid"
+              label="ID"
+              name="userid"
+              autoComplete="userid"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              로그인
+            </Button>
+            <Grid container sx={{ mt: 2, mb: 2 }}>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  비밀번호 찾기
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="register" variant="body2">
+                  {"회원가입"}
+                </Link>
+              </Grid>
+            </Grid>
+            <Button
+              color="kakao"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3 }}
+            >
+              카카오 로그인
+            </Button>
+            <Button
+              color="naver"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              네이버 로그인
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
