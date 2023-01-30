1e2eb70ced5eafe5a6b01a4bd8f0f57d4def0ba2
jsieon97 <rudtn0820@naver.com>
2023-01-30 11:24:46 +0900

    키오스크배포테스트

--- Frontend/shopmobile/src/components/userinfo/PaymentDetail.js
+++ Frontend/shopmobile/src/components/userinfo/PaymentDetail.js
@@ -1,28 +1,36 @@
 import React from 'react';
 
 const PaymentDetail = () => {
+  const products = [    { name: 'Product 1', quantity: 10, unitPrice: 50, numberic: true },    { name: 'Product 2', quantity: 5, unitPrice: 100 },  ];
+
+  const totalAmount = products.reduce((total, product) => {
+    return total + product.quantity * product.unitPrice;
+  }, 0);
+
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Product Name</th>
-          <th>Quantity</th>
-          <th>Unit Price</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Product 1</td>
-          <td>10</td>
-          <td>$50</td>
-        </tr>
-        <tr>
-          <td>Product 2</td>
-          <td>5</td>
-          <td>$100</td>
-        </tr>
-      </tbody>
-    </table>
+    <>
+      <div style={{ textAlign: 'right', padding: '10px' }}>
+        Total Purchase Amount: ${totalAmount}
+      </div>
+      <table style={{ width: '100%', borderCollapse: 'collapse' }}>
+        <thead>
+          <tr style={{ borderBottom: '1px solid #ddd', textAlign: 'left' }}>
+            <th style={{ padding: '10px' }}>Product Name</th>
+            <th style={{ padding: '10px' }}>Quantity</th>
+            <th style={{ padding: '10px' }}>Unit Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product, index) => (
+            <tr key={index} style={{ borderBottom: '1px solid #ddd', textAlign:product.numberic || false ? "left" : "right" }}>
+              <td style={{ padding: '10px' }}>{product.name}</td>
+              <td style={{ padding: '10px' }}>{product.quantity}</td>
+              <td style={{ padding: '10px' }}>${product.unitPrice}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 };
 
